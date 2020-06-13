eb705ec8adc9227511c5ba82f4a45141b0868a96
Scott Jones <scott2000jones@gmail.com>
2020-06-13 17:48:38 +0100

    progress on main.py CPU graphs

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import psutil
 from time import sleep
 from os import system
 from datetime import datetime
+import platform
 
+system("clear")
 t = Terminal()
-cpu_percents_history = []
 memory_history = []
 while True:
 
 
-    # Print titlebar with date and time
+    # Print titlebar with date, time, etc
     for i in range(0, t.width):
         with t.location(i,0):
             print("{t.bold}█{t.normal}".format(t=t), end='')
@@ -21,26 +22,43 @@ while True:
     with t.location(t.width-1-len(str(datetime.now())),0):
         print( ("{t.bold}{t.black}{t.on_white}"+ str(datetime.now()) + "{t.normal}").format(t=t))
     print('\n')
+    with t.location(t.width-1-len("Uptime: " + str(data.uptime())),2):
+        print(("{t.bold}" + "Uptime: " + str(data.uptime()) + "{t.normal}").format(t=t))
+    with t.location(t.width-1-len(str(platform.system()) + " | " + platform.release()),1):
+        print(str("{t.bold}" + platform.system() + " | " + platform.release()  + "{t.normal}").format(t=t))
+    print('\n')
 
     # Get current CPU data and add to history
     # If the history is storing more items than the terminal width, remove extra items
-    cpu_percents = data.cpu_percent_ints()
-    cpu_percents_history.insert(0, cpu_percents)
-    del cpu_percents_history[t.width:len(cpu_percents_history)-1]
+    # cpu_percents = data.cpu_percent_ints()
+    # cpu_percents_history.insert(0, cpu_percents)
+    # del cpu_percents_history[t.width:len(cpu_percents_history)-1]
 
-    # Print CPU usage chart
-    for i in range (1, t.width-2):
-        if i < len(cpu_percents_history):
-            thread_values = cpu_percents_history[i]
-            with t.location(t.width - 1 - i, 20 - int(round(list(map((lambda x: x/100 * (20)), thread_values))[0]))):
-                if int(round(list(map((lambda x: x/100 * (20)), cpu_percents_history[i]))[0])) < int(round(list(map((lambda x: x/100 * (20)), cpu_percents_history[i]))[0])):
-                    print("⠢")
-                else:
-                    print(".")
+    # Print CPU usage charts - probs bar graphs like htop because braille lines are more effort than they are worth tbh
+    currentThread = 0
+    cpu_percents = data.cpu_percent_ints()
+    while currentThread <= data.cpu_threads():
+        w = 1
+        h = 2 + (2 * currentThread)
+        if currentThread < 4:
+            w = 1
+        elif 4 <= currentThread < 8:
+            w = 5
+
+        with t.location(w, h):
+            print(str(currentThread) + ": [", end='')
+            for j in range (1, int(round(list(map((lambda x: x/100 * ((t.width/2)-12)), cpu_percents))[currentThread]))):
+                print("-", end='')
+
+
+        currentThread += 1
+        print('\n')
+        
     print('\n')
 
-    # Memory usage chart 
+    # Memory usage chart? Or just number
+    # PID list
+    # Historical graph of network usage - probs bar graph
     
-
     sleep(0.5)
     system("clear")
